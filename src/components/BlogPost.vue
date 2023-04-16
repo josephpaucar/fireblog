678f9d757c52af331d55cdd81a7fd37a668af4d1
Joseph Paucar <mailme@josephpaucar.dev>
2023-04-16 11:00:32 -0500

    BlogPost component created

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    post: {
+        type: Object
+    }
+}) 
+</script>
+
+<template>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+        <h2>{{ props.post.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,32 @@
 <script setup>
+import { ref, reactive } from 'vue';
+import BlogPost from '../components/BlogPost.vue';
+
+const welcomeScreen = ref([
+  {
+    hero: "Welcome!",
+    blogPost: "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+    welcomeScreen: true,
+    photo: "coding",
+  }
+])
+
+const sampleBlogPost = reactive([
+  {
+    title: "This is a filter Title",
+    blogHHTML: "This is a filter Title",
+    blogCoverPhoto: "beautiful-stories"
+  },
+  {
+    title: "This is a filter Title",
+    blogHHTML: "This is a filter Title",
+    blogCoverPhoto: "designed-for-everyone"
+  }
+])
 </script>
 
 <template>
-  <div class="text-3xl font-quicksand font-bold underline">
-    <h1>This the home page</h1>
-  </div>
+
+  <BlogPost :post="welcomeScreen" />
+  <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
 </template>
